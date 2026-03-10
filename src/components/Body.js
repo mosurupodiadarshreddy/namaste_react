e187afdfdb61753826d2ee238d249f30325a686d
Adarsh Reddy <adarshreddy713@gmail.com>
2026-03-10 23:22:50 +0530

    searching

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,7 +6,10 @@ import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
+    //when ever state variable updates react triggers reconciliation cycle(rerenders the compoennt) 
     const [ListofRestros,setListofRestros] = useState([]);
+    const [filteredRestros,setFilteredRestros] = useState([]);
+    const [searchText,setSearchText] = useState("");
     // const [allrestros,setAllrestros] = useState([]);
     const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0358&lng=77.5970&page_type=DESKTOP_WEB_LISTING";
 
@@ -24,6 +27,7 @@ const Body = () => {
             (card) => card?.card?.card?.gridElements?.infoWithStyle?.restaurants
             )?.card?.card?.gridElements?.infoWithStyle?.restaurants;
             setListofRestros(restrodata);
+            setFilteredRestros(restrodata);
         }
         catch(error)
         {
@@ -39,9 +43,7 @@ const Body = () => {
     //turnery operator conditional rendering
     return ListofRestros.length === 0 ? <Shimmer/> : (
     <div className="body">
-        <div className="search-bar">
-            {/* hello for search  */}
-        </div>
+        
         <div className="button-container">
             <button onClick={
                 function(){
@@ -50,11 +52,20 @@ const Body = () => {
                    setListofRestros(filteredlist);
                 }
             }> Top Restro's</button>
-
+            <div className="search-bar">
+                <input type="text" placeholder="search 🔍" className="searchbox" value={searchText} 
+                onChange={(e) => {setSearchText(e.target.value)}}></input>
+                <button className="search-button" onClick={() => {
+                    const searchedList = ListofRestros.filter(
+                        (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                    );
+                    setFilteredRestros(searchedList);
+                }}>Search</button>
+            </div>
         </div>
         <div className="res-cards-container">
             {
-              ListofRestros.map(
+              filteredRestros.map(
                 function(restrodata){
                   return <Restaurantcard key={restrodata.info.id} restaurantdata={restrodata}/>;
                 }
